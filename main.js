84384590f5b03414e135b8a71d54f38cf00e39e1
turhansel <turhansel@gmail.com>
2021-05-07 21:45:02 +0300

    initial comit

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      cart: 0,
+      product: "Socks",
+      brand: "Turti",
+      image: "./assets/images/socks_green.jpg",
+      inStock: false,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
+      ],
+    };
+  },
+
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      if (this.cart >= 1) {
+        this.cart -= 1;
+      }
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
+  },
+
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+  },
+});
